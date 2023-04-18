c10bce6ec8e4e2e16b38546921497a3dea36ad68
xiaohutuxian111 <2398391057@qq.com>
2023-04-18 15:02:55 +0800

    添加画布背景色

--- cainiaojiaocheng/python_base/alian_game/alien_invasion.py
+++ cainiaojiaocheng/python_base/alian_game/alien_invasion.py
@@ -15,12 +15,18 @@ def run_game():
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien  Invation")
 
+
+    # 设置背景色
+    bg_color =  (230,230,230)
+
     # 开始游戏的主循环
     while True:
         # 实时监控鼠标键盘事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        #  每次循环重新绘制屏幕
+        screen.fill(bg_color)
         #  让最近绘制的屏幕可见
         pygame.display.flip()
 
